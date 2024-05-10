6c9152b9f0a03e951a0da12b899b179a03f670f7
Muhammad Yahya <muhammad.yahya.asaan@gmail.com>
2024-05-11 01:27:42 +0500

    Share urls

--- resources/views/layouts/inc/social/horizontal.blade.php
+++ resources/views/layouts/inc/social/horizontal.blade.php
@@ -1,5 +1,4 @@
 <div class="text-center mt-4 mb-4 ms-0 me-0">
-	<button class='btn btn-fb share s_facebook' href="{{ config('settings.social_link.facebook_page_url') }}"><i class="fab fa-facebook"></i> </button>&nbsp;
-	<button class='btn btn-fb share s_instagram' href="{{ config('settings.social_link.instagram_url') }}"><i class="fab fa-instagram"></i> </button>&nbsp;
-	<button class='btn btn-lkin share s_linkedin' href="{{ config('settings.social_link.linkedin_url') }}"><i class="fab fa-linkedin"></i> </button>
+	<button class='btn btn-fb share s_facebook'><i class="fab fa-facebook"></i> </button>&nbsp;
+	<button class='btn btn-lkin share s_linkedin'><i class="fab fa-linkedin"></i> </button>
 </div>
