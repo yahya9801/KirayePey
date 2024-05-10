746d3139204df5120de900e44ffd7ea3ba4533b6
Muhammad Yahya <muhammad.yahya.asaan@gmail.com>
2024-05-11 01:17:24 +0500

    social

--- resources/views/layouts/inc/social/horizontal.blade.php
+++ resources/views/layouts/inc/social/horizontal.blade.php
@@ -1,4 +1,5 @@
 <div class="text-center mt-4 mb-4 ms-0 me-0">
 	<button class='btn btn-fb share s_facebook'><i class="fab fa-facebook"></i> </button>&nbsp;
+	<button class='btn btn-fb share s_instagram'><i class="fab fa-facebook"></i> </button>&nbsp;
 	<button class='btn btn-lkin share s_linkedin'><i class="fab fa-linkedin"></i> </button>
 </div>
