3902a20fd734aae44424bc51a44ee636557f312c
Muhammad Yahya <muhammad.yahya.asaan@gmail.com>
2024-06-15 16:38:40 +0500

    featured ad

--- resources/views/post/createOrEdit/inc/packages.blade.php
+++ resources/views/post/createOrEdit/inc/packages.blade.php
@@ -54,7 +54,7 @@
 											data-bs-placement="right"
 											data-bs-toggle="tooltip"
 											title="{!! $package->description_string !!}"
-									>{!! $package->name . $badge . " | $package->description_string "!!} </strong>
+									>{!! $package->name . $badge . " | $package->description "!!} </strong>
 								</label>
 							</div>
 						</td>
